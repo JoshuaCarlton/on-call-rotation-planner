f158879edf73f75dc05d2947725cf3f8254ae304
JoshuaCarlton <joshuancarlton@gmail.com>
2026-08-24 21:22:37 -0600

    cant add empty users

--- code/save_user.py
+++ code/save_user.py
@@ -12,9 +12,19 @@ def save_user(name_string, phone_string, initials_string, output_string, users):
     if check_for_duplicates(name):
         output_string.set("error: new user name cannot be the same as existing user")
         return
+    if phone == "":
+        output_string.set("error: phone number must be set")
+        return
+    if initials == "":
+        output_string.set("error: initials must be set")
+        return
     if not phone.isdigit():
         output_string.set("error: phone number must contain only numbers")
         return
+    if name == "":
+        output_string.set("error: name must be set")
+        return
+
     content = content + f"{name} {phone} {initials}\n"
     with open(abs_users, "w") as f:
         f.write(content)
